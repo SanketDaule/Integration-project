1f94e1e7eb0688bc8d810aa5d7448bd2241c561f
sanket daule <sanketdaule25@gmail.com>
2025-01-30 12:12:42 +0530

    ec2 instance code

---
+++ terraform/environments/dev/dev.tfvars
@@ -0,0 +1,2 @@
+ami           = "ami-0c614dee691cbbf37"
+instance_type = "t2.micro"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,16 @@
 variable "region" {
-    description = "default region"
-    type        = string
-    default     = "ap-south-1"
+  description = "default region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ami" {
+  description = "ami of the ec2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "instance type of ec2"
+  type        = string
+
 }
