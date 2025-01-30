bba806d5eb2f343156d7205dc2cef096aff049be
sanket daule <sanketdaule25@gmail.com>
2025-01-30 12:11:43 +0530

    update terraform version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "1.8.5"
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "5.41.0"
-      }
+  required_version = ">=1.8.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50.0"
     }
+  }
 }
